a0c4037ac39979603dcf151693a4aa560a14cb57
mihaialexandru <alex.mihai.email@gmail.com>
2022-08-28 09:50:21 +0300

    Implement get computer choice functionality

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let randomChoice = Math.floor(Math.random() * 3);
+    return choices[randomChoice];
+}
