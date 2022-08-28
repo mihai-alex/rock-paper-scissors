772937042d6dcf5e907520c898a8d9875d07a1e8
mihaialexandru <alex.mihai.email@gmail.com>
2022-08-28 15:14:15 +0300

    Implement input validation & minor refactoring

--- app.js
+++ app.js
@@ -4,53 +4,75 @@ function getComputerChoice() {
     return choices[randomChoice];
 }
 
+function validatePlayerChoice(playerSelection) {
+    if (playerSelection === null || playerSelection === undefined) {
+        return playerSelection;
+    }
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.trim();
+    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+        return playerSelection;
+    }
+    return undefined;
+}
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock") {
-        if (computerSelection == "rock") {
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
             return "TIE!";
         }
-        if (computerSelection == "paper") {
+        if (computerSelection === "paper") {
             return "YOU LOSE! Paper beats rock.";
         }
-        if (computerSelection == "scissors") {
+        if (computerSelection === "scissors") {
             return "YOU WIN! Rock beats scissors.";
         }
     }
-    if (playerSelection == "paper") {
-        if (computerSelection == "rock") {
+    if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
             return "YOU WIN! Paper beats rock.";
         }
-        if (computerSelection == "paper") {
+        if (computerSelection === "paper") {
             return "TIE!";
         }
-        if (computerSelection == "scissors") {
+        if (computerSelection === "scissors") {
             return "YOU LOSE! Scissors beats paper.";
         }
     }
-    if (playerSelection == "scissors") {
-        if (computerSelection == "rock") {
+    if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
             return "YOU LOSE! Rock beats scissors."
         }
-        if (computerSelection == "paper") {
+        if (computerSelection === "paper") {
             return "YOU WIN! Scissors beats paper."
         }
-        if (computerSelection == "scissors") {
+        if (computerSelection === "scissors") {
             return "TIE!";
         }
     }
 }
 
-//TODO: break this big "game" function into smaller functions (single responsibility)
-//TODO: add input validation
-//TODO: declare often used strings as global constants
 function game() {
     const numRounds = 5;
     let playerWins = 0;
     let computerWins = 0;
-    console.log(`The score is: ${playerWins} - ${computerWins} (player - computer)`);
+    console.log("Rock, paper, scissors, GO!!!")
     for (let round = 1; round <= numRounds; round++) {
         console.log(`Round ${round}:`);
+        console.log(`The score is: ${playerWins} - ${computerWins} (player - computer)`);
         let playerSelection = prompt("Choose between: rock, paper, scissors");
+        playerSelection = validatePlayerChoice(playerSelection);
+        if (playerSelection === undefined) {
+            console.log("The entered choice is not valid! Choose between: rock, paper, scissors");
+            round--;
+            continue;
+        }
+        if (playerSelection === null) {
+            console.log("You have exited the game. Refresh the page to play again!");
+            return;
+        }
+        
+
         let computerSelection = getComputerChoice();
         console.log("The computer chose: " + computerSelection);
         let roundResult = playRound(playerSelection, computerSelection);
@@ -58,15 +80,15 @@ function game() {
         if (roundResult.includes("WIN")) {
             playerWins++;
         }
-        else if(roundResult.includes("LOSE")) {
+        else if (roundResult.includes("LOSE")) {
             computerWins++;
         }
         else {
             playerWins++;
             computerWins++;
         }
-        console.log(`The score is: ${playerWins} - ${computerWins} (player - computer)`);
     }
+    console.log(`The score is: ${playerWins} - ${computerWins} (player - computer)`);
     if (playerWins == computerWins) {
         console.log("The game finished in a TIE.");
     }
@@ -77,3 +99,5 @@ function game() {
         console.log("YOU WON the game!");
     }
 }
+
+game();
