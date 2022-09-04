e4f8c6c8eb224e35fb5988d0fb9861e8e6215c0e
mihaialexandru <alex.mihai.email@gmail.com>
2022-09-04 13:50:16 +0300

    Do minor code cleanup

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let playerScore = 0;
 let computerScore = 0;
-let numberOfWinningRounds = 5;
+let numberOfRoundWins = 5;
 
 function updateScore() {
     document.getElementById('player-score').textContent = playerScore;
     document.getElementById('computer-score').textContent = computerScore;
-    if (playerScore >= numberOfWinningRounds || computerScore >= numberOfWinningRounds) {
+    if (playerScore >= numberOfRoundWins || computerScore >= numberOfRoundWins) {
         endGame();
     }
 }
 
 function updateComputerChoice(choice) {
-    document.getElementById('computer-choice').textContent = choice;;
+    document.getElementById('computer-choice').textContent = choice;
 }
 
 function updateStatus(status) {
@@ -19,36 +19,36 @@ function updateStatus(status) {
 }
 
 function updateRoundResultColor(result) {
-    const roundResult = document.querySelector(".round-result");
+    const roundResult = document.querySelector('.round-result');
     const playerSpan = document.getElementById('player-score');
     const computerSpan = document.getElementById('computer-score');
-    computerSpan.style.cssText = "transform: scale(1);";
-    playerSpan.style.cssText = "transform: scale(1);";
+    computerSpan.style.cssText = 'transform: scale(1);';
+    playerSpan.style.cssText = 'transform: scale(1);';
     switch (result) {
         case 1:
-            playerSpan.style.cssText = "transform: scale(1.5);";
+            playerSpan.style.cssText = 'transform: scale(1.5);';
             roundResult.style.cssText = 'border: .4rem solid green; \
                                         box-shadow: 0 0 1rem green;';
             break;
         case -1:
-            computerSpan.style.cssText = "transform: scale(1.5);";
+            computerSpan.style.cssText = 'transform: scale(1.5);';
             roundResult.style.cssText = 'border: .4rem solid red; \
                                         box-shadow: 0 0 1rem red;';
             break;
         case 0:
-            playerSpan.style.cssText = "transform: scale(1.5);";
-            computerSpan.style.cssText = "transform: scale(1.5);";
+            playerSpan.style.cssText = 'transform: scale(1.5);';
+            computerSpan.style.cssText = 'transform: scale(1.5);';
             roundResult.style.cssText = 'border: .4rem solid yellow; \
                                         box-shadow: 0 0 1rem yellow;';
             break;
         default:
             roundResult.style.cssText = 'border: .4rem solid gray; \
-            box-shadow: 0 0 1rem gray;';
+                                        box-shadow: 0 0 1rem gray;';
     }
 }
 
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
+    const choices = ['rock', 'paper', 'scissors'];
     let randomChoice = Math.floor(Math.random() * 3);
     return choices[randomChoice];
 }
@@ -60,36 +60,36 @@ function getComputerChoice() {
          1, player wins
 */
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock") {
-        if (computerSelection === "rock") {
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'rock') {
             return 0;
         }
-        if (computerSelection === "paper") {
+        if (computerSelection === 'paper') {
             return -1;
         }
-        if (computerSelection === "scissors") {
+        if (computerSelection === 'scissors') {
             return 1;
         }
     }
-    if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
             return 1;
         }
-        if (computerSelection === "paper") {
+        if (computerSelection === 'paper') {
             return 0;
         }
-        if (computerSelection === "scissors") {
+        if (computerSelection === 'scissors') {
             return -1;
         }
     }
-    if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
             return -1;
         }
-        if (computerSelection === "paper") {
+        if (computerSelection === 'paper') {
             return 1;
         }
-        if (computerSelection === "scissors") {
+        if (computerSelection === 'scissors') {
             return 0;
         }
     }
@@ -105,67 +105,68 @@ function choiceButtonsEventListenerFunction(e) {
         case -1:
             computerScore++;
             updateStatus(`You LOSE! ${computerSelection} beats ${playerSelection}!`);
-            updateScore();
             break;
         case 0:
             playerScore++;
             computerScore++;
             updateStatus(`TIE!`)
-            updateScore();
             break;
         case 1:
             playerScore++;
             updateStatus(`You WIN! ${playerSelection} beats ${computerSelection}!`);
-            updateScore();
             break;
     }
+    updateScore();
 }
 
 function addChoiceButtonsEvents() {
-    const choiceButtons = Array.from(document.getElementsByClassName("choice-button"));
-    choiceButtons.forEach(button => button.addEventListener('click', choiceButtonsEventListenerFunction));
+    const choiceButtons = Array.from(document.getElementsByClassName('choice-button'));
+    choiceButtons.forEach(button => button.addEventListener('click',
+        choiceButtonsEventListenerFunction));
 }
 
 function removeChoiceButtonsEvents() {
-    const choiceButtons = Array.from(document.getElementsByClassName("choice-button"));
-    choiceButtons.forEach(button => button.removeEventListener('click', choiceButtonsEventListenerFunction));
+    const choiceButtons = Array.from(document.getElementsByClassName('choice-button'));
+    choiceButtons.forEach(button => button.removeEventListener('click',
+        choiceButtonsEventListenerFunction));
 }
 
 function updateSubtitle(text) {
-    subtitle = document.getElementById("subtitle").textContent = text;
+    subtitle = document.getElementById('subtitle').textContent = text;
 }
 
 function play() {
     playerScore = 0;
     computerScore = 0;
-    numberOfWinningRounds = 5;
-    updateSubtitle(`First to win ${numberOfWinningRounds} rounds, wins the game!`);
+    numberOfRoundWins = 5;  // hardcoded number of round to play
+    updateSubtitle(`First to win ${numberOfRoundWins} rounds, wins the game!`);
     updateRoundResultColor(undefined);
     updateScore(undefined);
-    updateComputerChoice("The computer is");
-    updateStatus("waiting for your move...");
+    updateComputerChoice('The computer is');
+    updateStatus('waiting for your move...');
     addChoiceButtonsEvents();
 }
 
 function endGame() {
     removeChoiceButtonsEvents();
     if (playerScore > computerScore) {
-        updateSubtitle("Game over! You WON!");
-        subtitle = document.getElementById("subtitle").style.color = "green";
+        updateSubtitle('Game over! You WON!');
+        subtitle = document.getElementById('subtitle').style.color = 'green';
     }
     else if (computerScore > playerScore) {
-        updateSubtitle("Game over! You LOST!");
-        subtitle = document.getElementById("subtitle").style.color = "red";
+        updateSubtitle('Game over! You LOST!');
+        subtitle = document.getElementById('subtitle').style.color = 'red';
     }
     else {
-        updateSubtitle("Game over! It's a TIE!");
-        subtitle = document.getElementById("subtitle").style.color = "gold";
+        updateSubtitle('Game over! It\'s a TIE!');
+        subtitle = document.getElementById('subtitle').style.color = 'gold';
     }
+
     // refresh button
-    const title = document.getElementById("title");
-    title.textContent = "CLICK HERE TO PLAY AGAIN!";
-    title.style.color = "goldenrod";
-    title.href = "javascript:window.location.href=window.location.href";
+    const title = document.getElementById('title');
+    title.textContent = 'CLICK HERE TO PLAY AGAIN!';
+    title.style.color = 'goldenrod';
+    title.href = 'javascript:window.location.href=window.location.href';
 }
 
 play();
